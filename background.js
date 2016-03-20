fa5c3091f75fdc3fa083a7eb15b39f02962b9295
faure <hi@faure.hu>
2016-03-20 00:13:49 +0000

    Fixed issue where disconnected ports where not being removed from list of connected ports

--- background.js
+++ background.js
@@ -1,25 +1,29 @@
 // Connect with the devtool process
-var ports = [];
-chrome.runtime.onConnect.addListener(function(newPort) {
-  if (newPort.name !== "Paw") return;
+const ports = [];
+chrome.runtime.onConnect.addListener(function onConnect(newPort) {
+  if (newPort.name !== 'Paw') {
+    return;
+  }
   ports.push(newPort);
 
   // Make sure ports disconnect clean
-  newPort.onDisconnect.addListener(function() {
-      var i = ports.indexOf(port);
-      if (i !== -1) ports.splice(i, 1);
+  newPort.onDisconnect.addListener(function disconnectListener() {
+    const i = ports.indexOf(newPort);
+    if (i !== -1) {
+      ports.splice(i, 1);
+    }
   });
 
   // When message received from devtool process, send the cookies
-  newPort.onMessage.addListener(function(msg) {
-    if(msg.started) {
+  newPort.onMessage.addListener(function onMessageListener(msg) {
+    if (msg.started) {
       chrome.cookies.getAll({}, sendCookies);
     }
   });
 });
 
 function sendCookies(cookies) {
-  ports.forEach(function(port) {
-      port.postMessage(cookies);
+  ports.forEach(function sendMessage(port) {
+    port.postMessage(cookies);
   });
 }
